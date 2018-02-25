e06e70210081f1f5596f843ad065fd27cccf1945
Miwa <paintfx@gmail.com>
2018-02-25 07:15:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,18 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :index
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirates = Pirates.all
+      erb :show
+    end
 
   end
 end
